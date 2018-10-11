3de4e3331ab0cfcc095f141c519345c3a091c403
DARWINMATURAN\MaturaN <maturan72@gmail.com>
2018-10-11 08:28:21 -0600

    no message

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Users;
 
 class ApiController extends Controller
 {
-    //
+     public function UsersApi()
+    {
+        $data = Users::all();
+        return response()->json($data);;
+    }
 }
