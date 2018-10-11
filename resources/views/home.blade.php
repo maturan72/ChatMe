64ea70900d56476a40115728f97e448fd85f0f34
Daniel Fabular <fabulardaniel27@gmail.com>
2018-10-11 10:22:33 -0600

    no message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,58 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container home">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">ChatMe</div>
+                <div class="panel-heading">Chat Me</div>
 
                 <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="col-md-4">
 
-                    You are logged in!
+                      <div class="users">
+                        <h4 class="text-center">All UserS</h4>
+                          
+                            <div>
+                              <span><a class="flex1 name" data-toggle="tab" href="#message">Chat</a></span>
+                            </div>
+                          
+                      </div>
+
+                      
+
+                    </div>
+                    <div class="col-md-8 message">
+                        <div class="tab-content">
+                            <h4>Message Now</h4>
+                           <br>
+                            
+                          <div id="message" class="tab-pane fade message_section">
+                              <form action="" method="POST" enctype="multipart/form-data">
+                                 {{ csrf_field() }}
+
+
+                                    <br>
+                                  <div class="inset_message">
+                                    <input type="hidden" name="sender" value="">
+                                    <input type="hidden" name="reciever" value="">
+                                    <input class="form-control" type="text" name="message" placeholder="Enter your message here!!">
+                                    <div class="text-right">
+                                      <br>
+                                    <input class="btn btn-info" type="submit" name="send" value="Send Message">
+                                    </div>
+                                  </div>
+                                  <br>
+                              </form>
+                          </div>
+                               
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+
 @endsection
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Profile</div>
+
+                  <div class="panel-body">
+                      <h3 class="text-center">Pofile</h3>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+
+                   
